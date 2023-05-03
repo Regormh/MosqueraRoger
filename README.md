7d319634b738e26d424c0d96113585f1ee9713ff
Regormh <91439668+Regormh@users.noreply.github.com>
2023-05-03 10:06:18 -0500

    mosquera archivos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MosqueraRoger
+# FrancisStalin
+actividad del primer parcila de gestion del software
---
+++ marco-central.php
@@ -0,0 +1,62 @@
+<div style="float:left; width:60%; margin:60px auto; padding:1vw; border-left:1px solid gray; border-right:1px solid gray;">
+   <div  style="width: 100%;  ">     
+    <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
+       <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left; ">
+        	<h1>¿Quién es Roger Mosquera.?</h1>
+       		 <hr>
+             
+          <div id="posteo" style="text-align: left; font-size:0.5vw;">
+                Posteado por: Roger Mosquera<br>
+                Fecha: 2023-05-03 <br>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div style="width: 100%; height:100%;" >
+           <div  style="text-align:justify; font-size: 20px;">
+                   <p class="eys-parrafo">Hola, yo soy Roger <b> Mosquera Heredia </b>  ,  estudiante en la UTELVT en septimo ciclo de la carrera de Tecnología de la Información.  
+          Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quieza sería  por esta capacidad que tiene estas dos áres del conocimiento de solucionar muchos probemas de la sociedad </p>
+                   
+ 
+
+   <div id="blog-seccion2"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis estudios</h2> 
+    
+
+
+                   <p class="eys-parrafo"> Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon y en el Manuel Nieto Cadena donde me gradue en el año 2018</p>
+                   <p class="eys-parrafo"> Por el momento me dedico a la progrmacion web, pero en un futuro quiero dedicarme a la seguridad informatica</p>
+ 
+   <div id="blog-seccion3"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mi perfil informático</h2> 
+    
+                   <p  class="eys-parrafo">Hoy en día creo sisemas web combinando HTML, CSS, JavaScrip y PHP; tambien aplicaciones para dispositivo androi para lo cual utilizo el framework android Studio, el cual utiliza el lenguaje de JAVA..</p> 
+                   
+                   
+   <div id="blog-seccion4"  style="height: 50px;"></div>
+
+    <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
+    
+
+                   <p   class="eys-parrafo"> Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo manuales de los leguajes de progración que namejo, y tambien muchos noticias de las ultimas tendencias en tecnología.Pero sin duda el Voleibol es mi deporte y hooby favorito.</p>
+       
+
+                   
+
+               <hr>
+               <div style=" bottom: 0; margin-bottom: 0;   width:100%; display: inline-block;" >
+                  <div style="width: 40%;  float: left;">
+                   <a href="#">&#171;   Previo</a>
+                  </div>
+
+                  <div style="width: 40%; float: right; text-align: right;">
+                   <a href="apendiendo-cpp">Programa para aprender C++ &#187;</a>
+                  </div>
+
+                </div>
+           </div>
+  </div> 
+
+  </div>
